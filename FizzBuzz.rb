dc06c75425d372261dd6ec3aa9ccde6c98943ea6
Kiran Randhawa Kukar <Kiran.Randhawa-Kukar@softwire.com>
2022-09-08 17:47:04 +0100

    FizzBuzz basic and extra rules, and ask user for max number. all working

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,5 +1,58 @@
 def fizzbuzz
-  puts 'Hello World'
+  def maximum_number_request
+    puts "Input a maximum number"
+    return gets.chomp.to_i
+  end
+
+  maximum_number = maximum_number_request
+
+  for i in 1..maximum_number
+    result = []
+    fizz = 'Fizz'
+    fezz = 'Fezz'
+    buzz = 'Buzz'
+    bang = 'Bang'
+    bong = 'Bong'
+    if i % 11 == 0
+      result << bong
+    elsif i % 3 == 0
+      result << fizz
+      if i % 5 == 0
+        result << buzz
+        if i % 7 == 0
+          result << bang
+        end
+      else
+        if i % 7 == 0
+          result << bang
+        end
+      end
+    elsif i % 5 == 0
+      result << buzz
+      if i % 7 == 0
+        result << bang
+      end
+    elsif i % 7 == 0
+      result << bang
+    elsif i % 13 == 0
+      result << fezz
+    else
+      result << i
+    end
+    if i % 13 == 0 && result[0] != fezz
+      if result[0] == fizz
+          result.insert(1,fezz)
+      else
+        result.insert(0,fezz)
+      end
+    end
+    if i % 17 == 0
+      result = result.reverse
+    end
+    puts result.join
+  end
+
+
 end
 
 fizzbuzz
